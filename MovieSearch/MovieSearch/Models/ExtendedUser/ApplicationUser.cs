6edd8417d08ff22b1c31ecb9835ce005151527d2
LifeIsMadness <Luckstriker073@gmail.com>
2020-10-02 16:21:35 +0300

    Updating user model.. Adding image as well.

--- MovieSearch/MovieSearch/Models/ExtendedUser/ApplicationUser.cs
+++ MovieSearch/MovieSearch/Models/ExtendedUser/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,7 +13,10 @@ namespace MovieSearch.Models
 
         public List<Review> Reviews { get; set; }
 
+        [Display(Name = "Movies viewed")]
         public int MoviesViewedCount { get; set; }
+
+        //public string Image { get; set; }
 
     }
 }
--- MovieSearch/MovieSearch/Models/Movie.cs
+++ MovieSearch/MovieSearch/Models/Movie.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -24,6 +25,7 @@ namespace MovieSearch.Models
         public string Country { get; set; }
 
         [DefaultValue(0.0)]
+        [Display(Name = "Overal Rating")]
         public float OveralRating { get; set; }
 
         public int GenreId { get; set; }
